389e3c54ea52aa05e3b7fe34e7beb55688cfc1cf
azizovmuhammadali <alico.777777@gmail.com>
2025-04-14 22:09:51 +0500

    Added login and registration routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\V1\api\Admin\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::middleware(['lang'])->group(function () {
+ Route::post('admin/register',[UserController::class,'register']);
+ Route::post('admin/login',[UserController::class,'login']);
+});
