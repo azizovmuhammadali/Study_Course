29455d6fbc1f3e3adb616a5643c802d6b4e2829f
azizovmuhammadali <alico.777777@gmail.com>
2025-04-15 16:44:01 +0500

    Added and updated routes for admins and users

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\V1\api\Admin\UserController;
+use App\Http\Controllers\V1\api\User\UserController as User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,21 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::middleware(['lang'])->group(function () {
- Route::post('admin/register',[UserController::class,'register']);
- Route::post('admin/login',[UserController::class,'login']);
+Route::prefix('admin')->group(function(){
+    Route::post('register',[UserController::class,'register']);
+    Route::post('login',[UserController::class,'login']);
+    Route::middleware(['admin','auth:sanctum'])->group(function(){
+        Route::get('logout',[UserController::class,'logout']);
+    });
+});
+  Route::prefix('user')->group(function(){
+    Route::post('register',[User::class,'register']);
+    Route::post('login',[User::class,'login']);
+    Route::middleware(['auth:sanctum'])->group(function(){
+      Route::get('show/{id}',[User::class,'show']);
+      Route::put('update/{id}',[User::class,'update']);
+      Route::delete('delete/{id}',[User::class,'delete']);
+      Route::get('logout',[User::class,'logout']);
+    });
+  });
 });
