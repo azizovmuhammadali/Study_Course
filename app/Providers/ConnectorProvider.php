c7c1197dc66b7cc0455689a1bee469ba4e6f7557
azizovmuhammadali <alico.777777@gmail.com>
2025-04-22 23:16:16 +0500

    Connected course interfaces and classes

--- app/Providers/ConnectorProvider.php
+++ app/Providers/ConnectorProvider.php
@@ -3,12 +3,16 @@
 namespace App\Providers;
 
 use App\Interfaces\Reposities\CategoryReposityInterface;
+use App\Interfaces\Reposities\CourseReposityInterface;
 use App\Interfaces\Reposities\UserReposityInterface;
 use App\Interfaces\Services\CategoryServiceInterface;
+use App\Interfaces\Services\CourseServiceInterface;
 use App\Interfaces\Services\UserServiceInterface;
 use App\Reposities\CategoryReposity;
+use App\Reposities\CourseReposity;
 use App\Reposities\UserReposity;
 use App\Services\CategoryService;
+use App\Services\CourseService;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +27,8 @@ class ConnectorProvider extends ServiceProvider
         $this->app->bind(UserServiceInterface::class,UserService::class);
         $this->app->bind(CategoryReposityInterface::class,CategoryReposity::class);
         $this->app->bind(CategoryServiceInterface::class,CategoryService::class);
+        $this->app->bind(CourseReposityInterface::class,CourseReposity::class);
+        $this->app->bind(CourseServiceInterface::class,CourseService::class);
     }
 
     /**
